8f82741bed63b62b4014dd8dfa91ae3b208ad60a
Michael Tillman <mnamllit@gmail.com>
2022-07-09 19:46:40 -0500

    Edit cors origin

--- api/settings.py
+++ api/settings.py
@@ -35,12 +35,12 @@ DJOSER = {
     'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly']
 }
 
-CORS_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
